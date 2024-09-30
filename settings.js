9a396aeff918eb0fc1fa32fd2c7a8e46b783e37d
Nabeelkhan1314 <nabeelkhan1314@yahoo.com>
2024-09-30 22:22:05 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "17828242018"
+global.ownername = "nabeel khan"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '17828242018'  //creator number
+global.ownername = 'nabeel khan' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
